50c81b3513b1efdd21068bded0dae7dda8edcf96
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-04 11:54:55 +0300

    Creating a new project (106)

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+});
+
+app.listen(process.env.port || 3000, () => {
+    console.log(`listening on port 3000`)
+});
+
+
