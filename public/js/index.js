c5fb66ccbe42a9cd2221bfa0dff2d8f5c2fb1c4d
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-10 13:32:19 +0300

    Styling the chat app (116)

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    const messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('')
     })
 });
 
@@ -41,13 +43,16 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser')
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location ...');
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
         console.log(position)
     }, function (err) {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('unable to fetch location')
     })
 });
--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
        console.log('message from frontend', message);
 
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        callback();
 
     });
 
