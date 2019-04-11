f429dd2242061acf5a9d9e9a072fbb267bb491c8
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-11 13:19:59 +0300

    Printing message timestamp (118)

--- playground/time.js
+++ playground/time.js
@@ -1,8 +1,20 @@
 const moment = require('moment');
 
-const date = moment();
+// const date = moment();
+//
+// date.add(1, "years").subtract(10,"months");
+//
+//
+// console.log(date.format('MMMM Do, YYYY'));
+// console.log(date.format('h:mm a'));
+//
+//
+const createdAt = 1234;
 
-date.add(1, "years").subtract(10,"months");
+const tade = moment(createdAt);
 
-console.log(date.format('MMMM Do, YYYY'));
-console.log(date.format('h:mm a'));
+console.log(tade.format('h:mm a'));
+
+const someTimeStamp = moment().valueOf();
+
+console.log(someTimeStamp);
