f9e61184d8b418a479ac98a691f0bbdcac9bb3be
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-12 13:14:01 +0300

    Autoscrolling (120)

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom() {
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('connect', function () {
     console.log('connected to server');
 
@@ -19,6 +35,8 @@ socket.on('newMessage', function (message) {
     });
     jQuery('#messages').append(html);
 
+    scrollToBottom();
+
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,14 +50,7 @@ socket.on('newLocationMessage', function (message) {
     });
     jQuery('#messages').append(html);
 
-
-    // const formattedTime = moment(message.createdAt).format('h:mm a');
-    // const li = jQuery('<li></li>');
-    // const a = jQuery('<a target="_blank">My current location</a>');
-    // li.text(`${message.from} ${formattedTime}: `);
-    // a.attr('href', message.url);
-    // li.append(a);
-    // jQuery('#messages').append(li)
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
