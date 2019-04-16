1b3261465a7613928839e276e1488038a95f5f06
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2019-04-16 11:32:01 +0300

    Storing Users With ES6 Classes Part II (125)

--- server/utils/users.js
+++ server/utils/users.js
@@ -21,6 +21,25 @@ class Users {
         return user
     }
 
+    removeUser(id) {
+        const user = this.getUser(id);
+        if (user) {
+            this.users = this.users.filter(user => user.id !== id)
+        }
+
+        return user
+
+    }
+
+    getUser(id) {
+        return this.users.filter(user => user.id === id)[0]
+    }
+
+    getUserList(room) {
+        const users = this.users.filter((user) => user.room === room);
+        return users.map((user) => user.name);
+    }
+
 
 }
 
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -5,6 +5,25 @@ const {Users} = require('./users');
 
 describe('Users', () => {
 
+    let users;
+
+    beforeEach('', () => {
+        users = new Users();
+        users.users = [{
+            id: 1,
+            name: 'Mike',
+            room: 'node course'
+        }, {
+            id: 2,
+            name: 'Jen',
+            room: 'react course'
+        }, {
+            id: 3,
+            name: 'Julie',
+            room: 'node course'
+        }]
+    });
+
     it('should add new user', function () {
 
         const users = new Users();
@@ -17,5 +36,43 @@ describe('Users', () => {
         const responseUser = users.addUser(user.id, user.name, user.room);
 
         assert.deepEqual(users.users, [user])
+    });
+
+    it('should return names for node course', function () {
+
+        const userList = users.getUserList('node course');
+
+        assert.deepEqual(userList, ['Mike', 'Julie'])
+    });
+
+    it('should return names for react course', function () {
+
+        const userList = users.getUserList('react course');
+
+        assert.deepEqual(userList, ['Jen'])
+    });
+
+    it('should remove a user', function () {
+        const user = users.removeUser(1);
+
+        assert.equal(user.id, 1);
+        assert.notInclude(users.users, user);
+    });
+
+    it('should not remove a user', function () {
+        const user = users.removeUser(5);
+
+        assert.isUndefined(user);
+        assert.equal(users.users.length, 3)
+    });
+
+    it('should find user', function () {
+        const user = users.getUser(1);
+        assert.deepEqual(user, users.users[0]);
+    });
+
+    it('should not find user', function () {
+        const user = users.getUser(5);
+        assert.isUndefined(user)
     });
 });
